37154d0a5cb794d8faa372917ed0158eafd77f1c
Tushar Kumar Pathak <tkpathak2505@gmail.com>
2025-09-19 00:48:13 +0530

    deleted Ecom folder

--- ECom/ECom-Backend/ecommerce/src/main/java/com/ECom/ecommerce/entities/AddressType.java
+++
@@ -1,7 +0,0 @@
-package com.ECom.ecommerce.entities;
-
-public enum AddressType {
-    HOME,
-    WORK,
-    OTHER
-}
--- ECom/ECom-Backend/ecommerce/src/main/java/com/ECom/ecommerce/entities/OrderStatus.java
+++
@@ -1,9 +0,0 @@
-package com.ECom.ecommerce.entities;
-
-public enum OrderStatus {
-    PENDING,      // order placed
-    PROCESSING,   // being prepared / packed
-    SHIPPED,      // handed over to delivery
-    DELIVERED     // completed
-
-}
